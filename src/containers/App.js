85594abe9544a8e1615e93355af1116869815260
Michael Qin <mike.qin@gmail.com>
2019-05-14 02:24:38 -0400

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -5,19 +5,43 @@ import Cockpit from "../components/cockpit/Cockpit";
 import Persons from "../components/person/Persons";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    console.log("[App.js] constructor");
+  }
+
   state = {
     persons: [
       { id: 100, name: "Michael", age: 38 },
       { id: 200, name: "Stephanie", age: 28 },
       { id: 300, name: "Amy", age: 21 }
     ],
-    showPersons: false
+    showPersons: false,
+    appTitle: this.props.appTitle
   };
+
+  static getDerivedStateFromProps(nextProps, prevState) {
+    console.log("[App.js] getDerivedStateFromProps", nextProps);
+    return prevState;
+  }
+
+  componentDidMount() {
+    console.log("[App.js] componentDidMount");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("[App.js] shouldComponentUpdate");
+    return true;
+  }
+
+  componentDidUpdate() {
+    console.log("[App.js] componentDidUpdate");
+  }
 
   nameChangedHandler = (event, id) => {
     // copy array
     const persons = [...this.state.persons];
-    // update person    
+    // update person
     const index = persons.findIndex(p => p.id === id);
     const person = {
       ...persons[index]
@@ -43,6 +67,8 @@ class App extends Component {
   };
 
   render() {
+    console.log("[App.js] render");
+
     let persons = null;
 
     if (this.state.showPersons) {
@@ -58,6 +84,7 @@ class App extends Component {
     return (
       <div className={styles.App}>
         <Cockpit
+          appTitle={this.state.appTitle}
           showPersons={this.state.showPersons}
           persons={this.state.persons}
           clicked={() => this.togglePersonsHandler()}
